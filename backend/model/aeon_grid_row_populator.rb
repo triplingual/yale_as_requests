f64353d5cd26b2c56db6dfa4790329892cbc8c37
Payten Giles <payten.giles@gmail.com>
2022-01-22 09:28:40 +1100

    Fix query error when call number search returns no Born Digital records

--- backend/model/aeon_grid_row_populator.rb
+++ backend/model/aeon_grid_row_populator.rb
@@ -146,27 +146,29 @@ class AeonGridRowPopulator
         .distinct
         .map {|result| JSONModel::JSONModel(:archival_object).uri_for(result[:id], :repo_id => result[:repo_id])}
 
-      born_digital_uris.each_slice(256) do |batch|
-        uri_query = AdvancedQueryBuilder.new
-        batch.each do |ao_uri|
-          uri_query.or('id', ao_uri, 'text', true)
-        end
+      unless born_digital_uris.empty?
+        born_digital_uris.each_slice(256) do |batch|
+          uri_query = AdvancedQueryBuilder.new
+          batch.each do |ao_uri|
+            uri_query.or('id', ao_uri, 'text', true)
+          end
 
-        search_params = base_search_params.merge({
-                                                   :type => ['archival_object'],
-                                                   :aq => uri_query.build,
-                                                 })
+          search_params = base_search_params.merge({
+                                                     :type => ['archival_object'],
+                                                     :aq => uri_query.build,
+                                                   })
 
-        Search.search(search_params, nil).fetch('results', []).each_slice(16) do |matching_aos|
-          ao_jsonmodels = matching_aos.map {|result|
-            JSONModel::JSONModel(:archival_object).from_hash(ASUtils.json_parse(result.fetch('json')),
-                                                             false, true)
-          }
+          Search.search(search_params, nil).fetch('results', []).each_slice(16) do |matching_aos|
+            ao_jsonmodels = matching_aos.map {|result|
+              JSONModel::JSONModel(:archival_object).from_hash(ASUtils.json_parse(result.fetch('json')),
+                                                               false, true)
+            }
 
-          merged_record_hashes = RecordInheritance.merge(URIResolver.resolve_references(ao_jsonmodels, @resolve_params))
+            merged_record_hashes = RecordInheritance.merge(URIResolver.resolve_references(ao_jsonmodels, @resolve_params))
 
-          matching_aos.zip(merged_record_hashes).each do |result, inherited_json|
-              @aeon_grid_rows << BornDigitalRow.new(result, inherited_json)
+            matching_aos.zip(merged_record_hashes).each do |result, inherited_json|
+                @aeon_grid_rows << BornDigitalRow.new(result, inherited_json)
+            end
           end
         end
       end
